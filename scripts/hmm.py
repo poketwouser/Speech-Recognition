4e9814f498a4e63a5c77b6a4222bd0aa4d5c21af
Kiran Kumar P <kirankumarp405@gmail.com>
2026-04-10 02:50:16 +0530

    HMM Done

---
+++ scripts/hmm.py
@@ -0,0 +1,8 @@
+        self.tol = tol
+        self.min_covar = min_covar
+        
+        # log_pi, log_A, means, covars
+        self.log_pi = None
+        self.log_A = None
+        self.means = None
+        self.covars = None
